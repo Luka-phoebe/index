5df24486d55ba216448b88ac9b70465968ce178b
luka-phoebe <phoebeluka0@gmail.com>
2022-11-02 14:15:34 +0100

    third commit

---
+++ luka.py
@@ -0,0 +1,15 @@
+
+
+class phoebe:
+            def __init__(self, firstname, lastname, age, status):
+                self.firstname = firstname
+                self.lastname = lastname
+                self.age = age
+                self.status = status
+
+def printname(self):
+    print(self.firstname, self.lastname, self.age, self.status)
+
+
+x = phoebe("luka", "ashini", 56, "marriage")
+print(x.firstname)
